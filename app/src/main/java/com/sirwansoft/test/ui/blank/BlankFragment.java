388ba2da605279c40747169ca2250cd0685df10a
soran <sirwanrahimi2020@gmail.com>
2019-10-17 01:54:57 +0330

    first index

---
+++ app/src/main/java/com/sirwansoft/test/ui/blank/BlankFragment.java
@@ -0,0 +1,27 @@
+package com.sirwansoft.test.ui.blank;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.sirwansoft.test.R;
+
+
+public class BlankFragment extends Fragment {
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_blank, container, false);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/sirwansoft/test/ui/gallery/GalleryFragment.java
@@ -0,0 +1,36 @@
+package com.sirwansoft.test.ui.gallery;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.sirwansoft.test.R;
+
+public class GalleryFragment extends Fragment {
+
+    private GalleryViewModel galleryViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        galleryViewModel =
+                ViewModelProviders.of(this).get(GalleryViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+
+
+
+
+
+
+
+        return root;
+    }
+}
